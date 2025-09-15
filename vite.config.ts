f79d4689da981a8df24bfb2112ed353c18b3e7e3
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-09-15 19:03:24 -0300

    Voltando o vite.config para o padrão.

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server:{
-     proxy: {
+    proxy:{
       '/api': {
-          target: 'https://one023b-backend-novo-5twg.onrender.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        }
+        target: process.env.API_URL,
+        changeOrigin: true,
+        //Rewrite the URL to render backend on render.com
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
+    }
   }
 })
