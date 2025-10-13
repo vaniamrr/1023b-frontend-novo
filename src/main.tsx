891cf190926d28ac1c965ae17d2c1dedd929cc6c
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-10-13 12:50:41 -0300

    Código do login

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter , Routes , Route} from 'react-router-dom'
+import Login from './componentes/login/login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/login" element={<p>Login</p>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
