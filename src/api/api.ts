0f8725b352bb47e5eec5249547ba4cfe9cd6af5b
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-10-20 08:27:05 -0300

    Commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:process.env.API_URL
+    baseURL:import.meta.env.API_URL
 })
 //Nós vamos criar um middleware para adicionar o token na requisição
 
